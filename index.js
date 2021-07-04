28985264bb9e31a5b42c85f078ccac17d640072f
trog <joe@rconjoe.com>
2021-07-04 08:10:30 +0000

    init socket.io

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ app.use(cors({
 }))
 const port = 3000
 const { spawn } = require('child_process')
+const io = require('socket.io')(8080, {
+    cors: { origin: "*" }
+})
+io.on('connection', (socket) => {
+    console.log('a user connected')
 
+    socket.on('message', (message) => {
+        console.log(message)
+        io.emit('message', `${message}`)
+    })
+})
 app.get('/', (req, res) => {
   console.log('request received')
-  const child = spawn('ls', ['-a'])
+  const child = spawn('ip', ['a'])
     child.stdout.on('data', (data) => {
         console.log(data)
         res.send(data)
